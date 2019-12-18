7dedc7ff97b26c6ec5a877a61cecf613ba5e8314
Cristian Talau <cristi_talau@sync.ro>
2019-12-18 13:50:04 +0200

    WA-1663: Serving SVG more securely

--- src/main/java/com/oxygenxml/sdksamples/svg/SvgServlet.java
+++ src/main/java/com/oxygenxml/sdksamples/svg/SvgServlet.java
@@ -27,26 +27,39 @@ public class SvgServlet extends WebappServletPluginExtension {
    */
   @Override
   public void doGet(HttpServletRequest httpRequest, HttpServletResponse httpResponse) throws ServletException, IOException {
-    
-    // The hash of the current wanted image.
-    String docId = httpRequest.getParameter("docId");
-    String elemId = httpRequest.getParameter("elemId");
-    
-    AuthorAccess authorAccess = EditingSessionContextManager.getDocument(docId);
-    if (authorAccess != null) {
-      EditingSessionContext editingContext = authorAccess.getEditorAccess().getEditingContext();
-      PerDocumentSvgCache svgCache = (PerDocumentSvgCache) editingContext.getAttribute(EditingSessionContextManager.SVG_CACHE);
-      
-      String xml = svgCache.getXmlFragment(Long.valueOf(elemId));
-      
-      // mime type, cache, image content
-      httpResponse.setHeader("Cache-Control", "max-age=31536000");
-      httpResponse.setHeader("Content-Type", MediaType.SVG_UTF_8.toString());
-      httpResponse.setHeader("Vary", "Accept-Encoding");
-      ByteStreams.copy(new ByteArrayInputStream(xml.getBytes(Charsets.UTF_8)), httpResponse.getOutputStream());
+    if (isOpenedInBrowserTab(httpRequest)) {
+      httpResponse.sendError(HttpServletResponse.SC_BAD_REQUEST, "Load SVG using an <img> tag.");
     } else {
-      httpResponse.sendError(HttpServletResponse.SC_NOT_FOUND, "SVG file was not found.");
+      // The hash of the current wanted image.
+      String docId = httpRequest.getParameter("docId");
+      String elemId = httpRequest.getParameter("elemId");
+      
+      AuthorAccess authorAccess = EditingSessionContextManager.getDocument(docId);
+      if (authorAccess != null) {
+        EditingSessionContext editingContext = authorAccess.getEditorAccess().getEditingContext();
+        PerDocumentSvgCache svgCache = (PerDocumentSvgCache) editingContext.getAttribute(EditingSessionContextManager.SVG_CACHE);
+        
+        String xml = svgCache.getXmlFragment(Long.valueOf(elemId));
+        
+        // mime type, cache, image content
+        httpResponse.setHeader("Cache-Control", "max-age=31536000");
+        httpResponse.setHeader("Content-Type", MediaType.SVG_UTF_8.toString());
+        httpResponse.setHeader("Vary", "Accept-Encoding");
+        httpResponse.setHeader("Content-Security-Policy", "sandbox");
+        ByteStreams.copy(new ByteArrayInputStream(xml.getBytes(Charsets.UTF_8)), httpResponse.getOutputStream());
+      } else {
+        httpResponse.sendError(HttpServletResponse.SC_NOT_FOUND, "SVG file was not found.");
+      }
     }
+  }
+  
+  /**
+   * @param httpRequest The HTTP request.
+   * @return <code>true</code> if the URL is for sure opened in a browser tab.
+   */
+  private boolean isOpenedInBrowserTab(HttpServletRequest httpRequest) {
+    String accept = httpRequest.getHeader("Accept");
+    return accept != null && accept.contains("text/html");
   }
       
   /**
